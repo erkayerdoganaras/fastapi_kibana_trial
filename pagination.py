27b4a9aafc3904e95b578ad35f8a3133e4c6336f
erkayerdoganaras <64603744+erkayerdoganaras@users.noreply.github.com>
2021-07-01 16:44:34 +0300

    Update pagination.py

--- pagination.py
+++ pagination.py
@@ -1,12 +1,13 @@
 #Trial Codes for PAgination only
 
-res= es.search(index="logstash-2021.06.26",scroll='2m',size=10,body=query_body)
+res= es.search(index="logstash-2021.06.26",scroll='2m',size=100,body=query_body)
 counter=0
 sid=res["_scroll_id"]
 scroll_size=res["hits"]["total"]
 scroll_size=scroll_size["value"]
+print(res["_scroll_id"])
 
-while(scroll_size>0 and counter<400):
+while(scroll_size>0):
     page=es.scroll(scroll_id=sid,scroll="10m")
     sid=page["_scroll_id"]
     scroll_size=len(page["hits"]["hits"])
